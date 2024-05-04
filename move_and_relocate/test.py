9da289245236a1b2151df637321b1adf626edefb
liangxinyue668 <wyliangxinyue@163.com>
2024-05-04 12:00:13 +0800

    revise

--- move_and_relocate/test.py
+++ move_and_relocate/test.py
@@ -12,11 +12,11 @@ for i in range(1, 6):
     row_1 = [] #文件2第一个表格的数据需要移动到第二个表格的值
     for j in range(1, 8):
         row_1.append(sheet_2.cell(i, j).value)
-    Data_1.append(row_1)
+    data_1.append(row_1)
 
-for i in range(len(Data_1)):
-    for j in range(len(Data_1[i])):
-        sheet_3.cell(i+8, j+1, Data_1[i][j])
+for i in range(len(data_1)):
+    for j in range(len(data_1[i])):
+        sheet_3.cell(i+8, j+1, data_1[i][j])
 
 data_2 = []
 
@@ -24,11 +24,11 @@ for i in range(1, 6):
     row_2 = [] #文件1数据复制到文件2
     for j in range(1, 8):
         row_2.append(sheet_1.cell(i, j).value)
-    Data_2.append(row_2)
+    data_2.append(row_2)
 
-for i in range(len(Data_2)):
-    for j in range(len(Data_2[i])):
-        sheet_3.cell(i+1, j+1, Data_2[i][j])
+for i in range(len(data_2)):
+    for j in range(len(data_2[i])):
+        sheet_3.cell(i+1, j+1, data_2[i][j])
 
 excel_2.save('./文件2.xlsx')
 
