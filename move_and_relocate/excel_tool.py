a20727ade4bbd4058915a749fe074359e8f371f6
liangxinyue668 <wyliangxinyue@163.com>
2024-06-02 13:36:59 +0800

    simply copy_data()

--- move_and_relocate/excel_tool.py
+++ move_and_relocate/excel_tool.py
@@ -10,16 +10,9 @@
 
 def copy_data(source_sheet, target_sheet, start_row_src, start_col_src, start_row_tgt, start_col_tgt, num_rows, num_cols):
 
-    data = []
-
     # 从源工作表读取数据
-    for i in range(start_row_src, start_row_src + num_rows):
-        row = []
-        for j in range(start_col_src, start_col_src + num_cols):
-            row.append(source_sheet.cell(i, j).value)
-        data.append(row)
-    
-    # 将数据写入目标工作表
-    for i in range(len(data)):
-        for j in range(len(data[i])):
-            target_sheet.cell(start_row_tgt + i, start_col_tgt + j, data[i][j])
+    for i in range(num_rows):
+        for j in range(num_cols):
+            # 将数据写入目标工作表
+            value =  source_sheet.cell(start_row_src + i, start_col_src + j).value
+            target_sheet.cell(start_row_tgt + i, start_col_tgt + j, value)
--- move_and_relocate/remove_2.py
+++ move_and_relocate/remove_2.py
@@ -1,7 +1,9 @@
 import openpyxl
 import excel_tool
 
-week_report = openpyxl.load_workbook('D:/数据/周报/周报/亲子周报2024-05-20-2024-05-26.xlsx')
+print('hello world')
+
+week_report = openpyxl.load_workbook('D:/software/python/python_excel/move_and_relocate/copy_data/亲子周报2024-05-20-2024-05-26.xlsx')
 week_report_family_data = week_report['亲子周度数据']
 week_report_family_inventory = week_report['亲子库存']
 week_report_family_country = week_report['亲子站点数据']
@@ -13,30 +15,31 @@ week_report_acc_inventory = week_report['配饰库存']
 week_report_acc_country = week_report['配饰站点数据']
 
 # ...
-week_pivot_table = openpyxl.load_workbook('D:/数据/周报/数据/亲子周报透视数据2024-05-20_2024-05-26.xlsx')
+week_pivot_table = openpyxl.load_workbook('D:/software/python/python_excel/move_and_relocate/copy_data/亲子周报透视数据2024-05-20_2024-05-26.xlsx')
 week_pivot_table_family_sales = week_pivot_table['亲子销售透视']
 week_pivot_table_family_inventory = week_pivot_table['亲子库存透视']
-week_pivot_table_shoes_sales = week_pivot_table['童鞋销售']
-week_pivot_table_shoes_inventory = week_pivot_table['童鞋库存']
-week_pivot_table_acc_sales = week_pivot_table['配饰销售']
-week_pivot_table_acc_inventory = week_pivot_table['配饰库存']
+# week_pivot_table_shoes_sales = week_pivot_table['童鞋销售']
+# week_pivot_table_shoes_inventory = week_pivot_table['童鞋库存']
+# week_pivot_table_acc_sales = week_pivot_table['配饰销售']
+# week_pivot_table_acc_inventory = week_pivot_table['配饰库存']
 
-week_country = openpyxl.load_workbook('D:/数据/周报/数据/站点商品数据2024-05-20_2024-05-26.xlsx')
+week_country = openpyxl.load_workbook('D:/software/python/python_excel/move_and_relocate/copy_data/站点商品数据2024-05-20_2024-05-26.xlsx')
 week_country_family = week_country['亲子']
-week_country_shoes = week_country['童鞋']
-week_country_acc = week_country['配饰']
+# week_country_shoes = week_country['童鞋']
+# week_country_acc = week_country['配饰']
 
 # 亲子周报
 # 新老品爆旺数据
-excel_tool.copy_data(week_report_family_data, week_report_family_data, 51, 2, 58, 2, 4, 15)
+# excel_tool.copy_data(week_report_family_data, week_report_family_data, 51, 2, 58, 2, 4, 15)
 
-excel_tool.copy_data(week_pivot_table_family_sales, week_report_family_data, 38, 2, 51, 2, 4, 15)
+# excel_tool.copy_data(week_pivot_table_family_sales, week_report_family_data, 38, 2, 51, 2, 4, 15)
 
 # 新老品销量层级
-excel_tool.copy_data(week_report_family_data, week_report_family_data, 75, 3, 97, 3, 17, 15)
+excel_tool.copy_data(week_report_family_data, week_report_family_data, 75, 3, 97, 3, 19, 15)
 
-excel_tool.copy_data(week_pivot_table_family_sales, week_report_family_data, 49, 3, 75, 3, 17, 15)
+# excel_tool.copy_data(week_pivot_table_family_sales, week_report_family_data, 49, 3, 75, 3, 17, 15)
 
+'''
 #三级类目销售
 excel_tool.copy_data(week_report_family_data, week_report_family_data, 143, 3, 168, 3, 21, 15)
 
@@ -83,8 +86,9 @@ excel_tool.copy_data(week_report_family_country, week_report_family_country, 5, 
 excel_tool.copy_data(week_report_family_country, week_report_family_country, 5, 8, 40, 8, 31, 4)
 excel_tool.copy_data(week_country_family, week_report_family_country, 5, 3, 5, 15, 31, 14)
 excel_tool.copy_data(week_country_family, week_report_family_country, 127, 3, 5, 8, 31, 4)
+'''
 
-week_report.save('D:/数据/周报/周报/亲子周报2024-05-20-2024-05-26.xlsx')
+week_report.save('D:/software/python/python_excel/move_and_relocate/copy_data/亲子周报2024-05-20-2024-05-26.xlsx')
 
 week_report.close()
 
