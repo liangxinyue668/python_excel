367cffadcc7fc4711b7132b6f37e99352f6315b0
zogodo <742782908@qq.com>
2024-06-19 11:18:40 +0800

    打印 csv tree OK

--- tree/tree.py
+++ tree/tree.py
@@ -1,10 +1,18 @@
 import openpyxl
 
-def PrintTree(level):
+id = 0
+
+def PrintTree(level, father):
+    global id
     if level == 5:
         return
-    print('A' * level + 'B')
-    PrintTree(level + 1)
-    PrintTree(level + 1)
+    title = 'A' * level + 'B'
+    id += 1
+    me = id
+    line = '子任务,%4d,%4d,%s,刘姿彤,周国栋,TR3' % (id, father, title)
+    print(line)
+    PrintTree(level + 1, me)
+    PrintTree(level + 1, me)
 
-PrintTree(0)
+print('类型,工作项ID,父级,摘要,经办人,报告人,迭代')
+PrintTree(0, 0)
